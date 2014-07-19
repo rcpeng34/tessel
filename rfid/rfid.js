266b2e77e0effe40b95e85ebd1a68934c4785958
rcpeng34 <rcpeng@gmail.com>
2014-07-19 12:11:19 -0700

    Finishes experiments with rfid and shell command

--- rfid/rfid.js
+++ rfid/rfid.js
@@ -7,8 +7,12 @@ device to come within range of the module,
 then logs its UID to the console.
 *********************************************/
 
+var child_process = require('child_process');
+child_process.exec('say "hello Patrick"', [], function(){console.log('ran it');});
+
 var tessel = require('tessel');
 var rfidlib = require('rfid-pn532');
+
 
 var rfid = rfidlib.use(tessel.port['D']); 
 
@@ -17,9 +21,21 @@ rfid.on('ready', function (version) {
 
   rfid.on('data', function(card) {
     console.log('UID:', card.uid.toString('hex'));
+    // run_cmd('say', ['hello patrick'], function(){console.log('shell callback');});
+    child_process.exec('say "hello Patrick"', [], function(){console.log('ran it');})
+ 
   });
 });
 
 rfid.on('error', function (err) {
   console.error(err);
 });
+
+
+var run_cmd = function (cmd, args, callBack ) {
+    var child = spawn(cmd, args);
+    var resp = "";
+
+    child.stdout.on('data', function (buffer) { resp += buffer.toString() });
+    child.stdout.on('end', function() { callBack (resp) });
+}
